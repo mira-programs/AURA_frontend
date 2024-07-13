a29a2a84768e54a2c6d2606b398b7a6641514744
mms161 <mms161@mail.aub.edu>
2024-07-13 15:42:24 +0300

    update
    fixed dp/sp usage in my files

--- Aura/app/src/main/java/com/auraXP/aura/MainActivity.java
+++ Aura/app/src/main/java/com/auraXP/aura/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Enable full-screen mode
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
