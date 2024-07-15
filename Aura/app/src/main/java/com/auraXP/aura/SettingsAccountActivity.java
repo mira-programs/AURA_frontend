603b2e09232bbf7c696fd188477b5047881b524b
mariaaeid <mariaeid20@gmail.com>
2024-07-16 02:16:54 +0300

    .

--- Aura/app/src/main/java/com/auraXP/aura/SettingsAccountActivity.java
+++ Aura/app/src/main/java/com/auraXP/aura/SettingsAccountActivity.java
@@ -1,6 +1,8 @@
 package com.auraXP.aura;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -27,6 +29,46 @@ public class SettingsAccountActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        notificationsButton = findViewById(R.id.notifications_button);
+        streaksButton = findViewById(R.id.streaks_button);
+        restrictedButton = findViewById(R.id.restricted_button);
+        datetimeButton = findViewById(R.id.datetime_button);
+
+
+
+        notificationsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Intent intent = new Intent(SettingsAccountActivity.this, ProfileAccountActivity.class);
+               // startActivity(intent);
+            }
+        });
+
+        streaksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Intent intent = new Intent (SettingsAccountActivity.this, AurapointsAccountActivity.class);
+              //  startActivity(intent);
+            }
+        });
+
+        restrictedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Intent intent = new Intent(SettingsAccountActivity.this, ProfileAccountActivity.class);
+              //  startActivity(intent);
+            }
+        });
+        
+        datetimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Intent intent = new Intent (SettingsAccountActivity.this, AurapointsAccountActivity.class);
+                // startActivity(intent);
+            }
         });
     }
 }
