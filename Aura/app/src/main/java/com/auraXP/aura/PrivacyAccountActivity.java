3d9e9fa811145b9a7aea220033bc9118b8afe4bf
mariaaeid <mariaeid20@gmail.com>
2024-07-16 01:44:36 +0300

    .

--- Aura/app/src/main/java/com/auraXP/aura/PrivacyAccountActivity.java
+++ Aura/app/src/main/java/com/auraXP/aura/PrivacyAccountActivity.java
@@ -1,6 +1,7 @@
 package com.auraXP.aura;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PrivacyAccountActivity extends AppCompatActivity {
+
+    private Switch switchh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,17 @@ public class PrivacyAccountActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        switchh = findViewById(R.id.switch1);
+
+
+        switchh.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+               //on
+            } else {
+                //off
+            }
         });
     }
 }
