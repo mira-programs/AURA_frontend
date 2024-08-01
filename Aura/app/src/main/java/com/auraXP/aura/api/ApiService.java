eff01eda9a63aa250123d6057a9941eb8f8f367f
mms161 <mms161@mail.aub.edu>
2024-08-01 14:05:51 +0300

    trying
    trying

--- Aura/app/src/main/java/com/auraXP/aura/api/ApiService.java
+++ Aura/app/src/main/java/com/auraXP/aura/api/ApiService.java
@@ -12,12 +12,16 @@ import com.auraXP.aura.api.models.FriendRequest;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.DELETE;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -82,5 +86,11 @@ public interface ApiService {
 
     @GET("/accounts/{account_id}/received_friend_requests")
     Call<List<FriendRequest>> getReceivedFriendRequests(@Path("account_id") int accountId);
+
+    @POST("/predict")
+    Call<GeminiResponse> processImage(
+            @Part MultipartBody.Part file,
+            @Part("description") RequestBody description
+    );
 }
 
