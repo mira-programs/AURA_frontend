464d9f5e248cf9b1edb373385171f79e436b4c76
mms161 <mms161@mail.aub.edu>
2025-09-14 19:41:03 +0300

    replacing linearlayouts and relativelayouts with constraintlayouts where cl is more useful

--- Aura/app/src/main/java/com/auraXP/aura/LoginActivity.java
+++ Aura/app/src/main/java/com/auraXP/aura/LoginActivity.java
@@ -11,31 +11,27 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.auraXP.aura.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
 
-    EditText etUsernameLogin;
-    EditText etPasswordLogin;
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        etUsernameLogin = findViewById(R.id.etUsernameLogin);
-        etPasswordLogin = findViewById(R.id.etPasswordLogin);
-
         Button loginButton = findViewById(R.id.btnLogin);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (validate()) {
-                    Intent intent = new Intent(LoginActivity.this, DailyChallenges.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(LoginActivity.this, "Please fill out all fields.", Toast.LENGTH_SHORT).show();
-                }
+        loginButton.setOnClickListener(view -> {
+            if (validate()) {
+                Intent intent = new Intent(LoginActivity.this, DailyChallenges.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(LoginActivity.this, "Please fill out all fields.", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -43,12 +39,12 @@ public class LoginActivity extends AppCompatActivity {
     public boolean validate() {
         boolean isValid = true;
 
-        if (TextUtils.isEmpty(etUsernameLogin.getText().toString())) {
-            etUsernameLogin.setError("Username is required.");
+        if (TextUtils.isEmpty(binding.etUsernameLogin.getText().toString())) {
+            binding.etUsernameLogin.setError("Username is required.");
             isValid = false;
         }
-        if (TextUtils.isEmpty(etPasswordLogin.getText().toString())) {
-            etPasswordLogin.setError("Password is required.");
+        if (TextUtils.isEmpty(binding.etPasswordLogin.getText().toString())) {
+            binding.etPasswordLogin.setError("Password is required.");
             isValid = false;
         }
 
--- Aura/app/src/main/java/com/auraXP/aura/MainActivity.java
+++ Aura/app/src/main/java/com/auraXP/aura/MainActivity.java
@@ -1,31 +1,44 @@
 package com.auraXP.aura;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
-import androidx.annotation.NonNull;
+import android.view.WindowInsetsController;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import com.auraXP.aura.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController insetsController = getWindow().getInsetsController();
+            if (insetsController != null) {
+                insetsController.hide(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     public void onClickGoLoginPage(View view) {
